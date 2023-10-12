5686e6ca9f989b3590652223321194385c7c620d
adamdullenty <adam.dullenty@gmail.com>
2023-10-12 14:32:37 +0100

    Add unhappy path tests

--- spec/post_nl/operations/create_shipment_spec.rb
+++ spec/post_nl/operations/create_shipment_spec.rb
@@ -20,75 +20,108 @@ RSpec.describe PostNL::Operations::CreateShipment, :aggregate_failures do
     )
   end
 
-  describe "#execute", vcr_cassette: "create_shipment_request_confirm_true" do
-    it "returns a hash" do
-      expect(subject.execute).to be_a(Hash)
+  describe "#execute" do
+    describe "successful requests", vcr_cassette: "create_shipment_request_confirm_true" do
+      it "returns a hash" do
+        expect(subject.execute).to be_a(Hash)
+      end
+
+      it "returns the ResponseShipments key" do
+        expect(subject.execute).to have_key(:ResponseShipments)
+      end
+
+      it "returns the Barcode (tracking number) key" do
+        response = subject.execute
+
+        expect(response.dig(:ResponseShipments, 0)).to have_key(:Barcode)
+      end
+
+      it "returns the Labels > Content key" do
+        response = subject.execute
+
+        expect(response.dig(:ResponseShipments, 0, :Labels, 0)).to have_key(:Content)
+      end
+
+      describe "label confirmation" do
+        before do
+          allow(http_client).to receive(:request).and_call_original
+        end
+
+        context "when confirm is not specified" do
+          subject do
+            described_class.new(
+              http_client: http_client,
+              payload: payload
+            )
+          end
+
+          it "includes ?confirm=true in the endpoint URL by default" do
+            subject.execute
+
+            expect(http_client).to have_received(:request).with(
+              http_method: :post,
+              path: "/shipment/v2_2/label?confirm=true",
+              payload: payload.to_json
+            )
+          end
+        end
+
+        context "with confirm: true option" do
+          subject do
+            described_class.new(
+              http_client: http_client,
+              payload: payload,
+              confirm: true
+            )
+          end
+
+          it "includes ?confirm=true in the endpoint URL" do
+            subject.execute
+
+            expect(http_client).to have_received(:request).with(
+              http_method: :post,
+              path: "/shipment/v2_2/label?confirm=true",
+              payload: payload.to_json
+            )
+          end
+        end
+
+        context "with confirm: false option", vcr_cassette: "create_shipment_request_confirm_false" do
+          subject do
+            described_class.new(
+              http_client: http_client,
+              payload: payload,
+              confirm: false
+            )
+          end
+
+          it "includes ?confirm=false in the requested endpoint path" do
+            subject.execute
+
+            expect(http_client).to have_received(:request).with(
+              http_method: :post,
+              path: "/shipment/v2_2/label?confirm=false",
+              payload: payload.to_json
+            )
+          end
+        end
+      end
     end
 
-    it "returns the ResponseShipments key" do
-      expect(subject.execute).to have_key(:ResponseShipments)
-    end
-
-    describe "label confirmation" do
-      before do
-        allow(http_client).to receive(:request).and_call_original
-      end
-
-      context "when confirm is not specified" do
-        subject do
-          described_class.new(
-            http_client: http_client,
-            payload: payload
+    describe "error handling" do
+      context "when there is a client error" do
+        let(:payload) { { broken: "json" } }
+        let(:expected_message_pattern) do
+          Regexp.new(
+            "^Client error occurred. Status code: 400.*" \
+            "The user is not authorized for this operation based on the provided CustomerCode"
           )
         end
 
-        it "includes ?confirm=true in the endpoint URL by default" do
-          subject.execute
-
-          expect(http_client).to have_received(:request).with(
-            http_method: :post,
-            path: "/shipment/v2_2/label?confirm=true",
-            payload: payload.to_json
-          )
-        end
-      end
-
-      context "with confirm: true option" do
-        subject do
-          described_class.new(
-            http_client: http_client,
-            payload: payload,
-            confirm: true
-          )
-        end
-
-        it "includes ?confirm=true in the endpoint URL" do
-          subject.execute
-
-          expect(http_client).to have_received(:request).with(
-            http_method: :post,
-            path: "/shipment/v2_2/label?confirm=true",
-            payload: payload.to_json
-          )
-        end
-      end
-
-      context "with confirm: false option", vcr_cassette: "create_shipment_request_confirm_false" do
-        subject do
-          described_class.new(
-            http_client: http_client,
-            payload: payload,
-            confirm: false
-          )
-        end
-
-        it "includes ?confirm=false in the requested endpoint path" do
-          subject.execute
-
-          expect(http_client).to have_received(:request).with(
-            http_method: :post,
-            path: "/shipment/v2_2/label?confirm=false",
-            payload: payload.to_json
+        it "raises a PostNL::Errors::ClientError", vcr_cassette: "create_shipment_request_client_error" do
+          expect { subject.execute }.to raise_error(
+            PostNL::Errors::ClientError,
+            expected_message_pattern
           )
         end
       end
